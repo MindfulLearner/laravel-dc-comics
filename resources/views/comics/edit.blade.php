2a5bda0c6b96c2d2ee441018e0216ac1409d62b7
MindfulLearner <dimajosh1009@gmail.com>
2024-10-23 21:52:13 +0000

    added stilization on form control

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -3,17 +3,41 @@
 @vite('resources/js/app.js')
 
 <div class="container-edit">
- <form action="{{ route('comics.update', $comic['id']) }}" method="POST">
+ <form action="{{ route('comics.update', $comic['id']) }}" method="POST" class="edit-form">
     @csrf
     @method('PUT')
-    <input type="text" name="title" value="{{ $comic['title'] }}">
-    <input type="text" name="description" value="{{ $comic['description'] }}">
-    <input type="text" name="image" value="{{ $comic['image'] }}">
-    <input type="text" name="series" value="{{ $comic['series'] }}">
-    <input type="text" name="price" value="{{ $comic['price'] }}">
-    <input type="text" name="type" value="{{ $comic['type'] }}">
-    <input type="text" name="artists" value="{{ $comic['artists'] }}">
-    <input type="text" name="writers" value="{{ $comic['writers'] }}">
-    <button type="submit">Edit</button>
+    <div class="form-group form-title">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" value="{{ $comic['title'] }}" class="form-control">
+    </div>
+    <div class="form-group form-description">
+        <label for="description">Description</label>
+        <input type="text" name="description" id="description" value="{{ $comic['description'] }}" class="form-control">
+    </div>
+    <div class="form-group form-image">
+        <label for="image">Image</label>
+        <input type="text" name="image" id="image" value="{{ $comic['image'] }}" class="form-control">
+    </div>
+    <div class="form-group form-series">
+        <label for="series">Series</label>
+        <input type="text" name="series" id="series" value="{{ $comic['series'] }}" class="form-control">
+    </div>
+    <div class="form-group form-price">
+        <label for="price">Price</label>
+        <input type="text" name="price" id="price" value="{{ $comic['price'] }}" class="form-control">
+    </div>
+    <div class="form-group form-type">
+        <label for="type">Type</label>
+        <input type="text" name="type" id="type" value="{{ $comic['type'] }}" class="form-control">
+    </div>
+    <div class="form-group form-artists">
+        <label for="artists">Artists</label>
+        <input type="text" name="artists" id="artists" value="{{ $comic['artists'] }}" class="form-control">
+    </div>
+    <div class="form-group form-writers">
+        <label for="writers">Writers</label>
+        <input type="text" name="writers" id="writers" value="{{ $comic['writers'] }}" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Edit</button>
  </form>
 </div>
